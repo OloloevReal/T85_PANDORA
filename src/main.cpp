c0216723635236e91af509c13394feefdcdcae7a
Nikolay <nickey.n@gmail.com>
2022-04-20 14:41:40 +0300

    a lot of changes

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,13 @@
 #include "sensor.h"
 
 #define VERSION "0.0.1"
-#define LED1 PIN_B1
+
+#define PIN_BUTTON PIN_B0
+
+#define PIN_LED1 PIN_B4
+
+#define PIN_VALET PIN_B1
+
 #define PIN_SENSOR PIN_B5
 #define ADC_SENSOR 0
 
@@ -29,12 +35,19 @@ Sensor_Real * _sensor = new  Sensor_Real(PIN_SENSOR, ADC_SENSOR);
 void setup(){
     Serial.begin(9600);
     
-    DDRB |= (1 << DDB1) | (1 << DDB0);
-    PORTB &= ~(1 << PB1);
+    DDRB |= (1 << PIN_LED1) | (1 << PIN_VALET) | (1 << PIN_BUTTON); // OUTPUT
+    PORTB &= ~_BV(PIN_LED1);  // LOW
+    PORTB &= ~_BV(PIN_VALET); // LOW
+    PORTB |= _BV(PIN_BUTTON); // HIGHT
 
-    pinMode(PIN_B0, OUTPUT);
-    digitalWrite(PIN_B0, HIGH);
-    if(!digitalRead(PIN_B0)){
+    // pinMode(PIN_LED1, OUTPUT);
+    // digitalWrite(PIN_LED1, LOW);
+
+    // pinMode(PIN_VALET, OUTPUT);
+
+    // pinMode(PIN_BUTTON, OUTPUT);
+
+    if(!digitalRead(PIN_BUTTON)){
         Serial.println("Storage clearing");
        _storage.clear();
     }
@@ -56,24 +69,27 @@ void setup(){
     _sensor->Init();
 }
 
-void flashLed(uint8_t v, uint8_t mult = 1, bool _delay = true){
-    for (size_t i = 0; i < v; i++)
+void flashLed(uint8_t _pin, uint8_t n, uint8_t mult = 1, bool _post_delay = true){
+    for (size_t i = 0; i < n; i++)
     {
-        PORTB ^=(1<<PB1);
-        delay((300 * mult)); // TODO: change to _delay_ms
-        PORTB ^=(1<<PB1);
-        delay((200 * mult));
+        PORTB ^=_BV(_pin);
+        delay((150 * mult)); // TODO: change to _delay_ms
+        PORTB ^=_BV(_pin);
+        delay((100 * mult));
     }
-    if(_delay){
+    if(_post_delay){
         _delay_ms(1000);
     }
 }
 
-void pushValet(uint8_t num){
-        Serial.printf(F("%d "), num);
-        flashLed(num, 1, false);
+uint8_t pushValet(uint8_t num){
+        Serial.printf("%d ", num);
+        flashLed(PIN_VALET, num, 1, false);
         //Serial.printf("Impuls: %d\r\n", _sensor->GetImpuls());
-        _delay_ms(500);
+        //_delay_ms(500);
+        uint8_t ack = _sensor->GetImpulse();
+        Serial.printf("(%d) ", ack);
+        return ack;
 }
 
 void loop(){
@@ -81,33 +97,42 @@ void loop(){
     {
         if(_data._m == STOP){
             //print result
-            flashLed(_digs->n0, 2);
+            flashLed(PIN_LED1,_digs->n0, 2);
             //_delay_ms(t_delay);
-            flashLed(_digs->n1, 2);
+            flashLed(PIN_LED1,_digs->n1, 2);
             //_delay_ms(t_delay);
-            flashLed(_digs->n2, 2);
+            flashLed(PIN_LED1,_digs->n2, 2);
             //_delay_ms(t_delay);
-            flashLed(_digs->n3, 2, false);
+            flashLed(PIN_LED1,_digs->n3, 2, false);
         }
         _delay_ms(5000);
         return -1;
     }
-    
+        //flashLed(PIN_LED1, 2, 2, false);
         Serial.printf("->: %d\r\n", _digs->Next());
         //Serial.printf("PIN0: %d\r\n", _sensor->GetValue());
         
+        flashLed(PIN_LED1, 1, 2, false);
         pushValet(_digs->n0);
+
+        flashLed(PIN_LED1, 1, 2, false);
         pushValet(_digs->n1);
+
+        flashLed(PIN_LED1, 1, 2, false);
         pushValet(_digs->n2);
-        pushValet(_digs->n3);
+
+        flashLed(PIN_LED1, 1, 2, false);
+        uint8_t ack = pushValet(_digs->n3);
 
         Serial.println();
 
-        Serial.print("Impulse: "); 
-        uint8_t _value = _sensor->GetImpulse();
-        Serial.println(_value);
-        if(_value == 2){
+        // Serial.print("Impulse: "); 
+        // uint8_t _value = _sensor->GetImpulse();
+        // Serial.println(_value);
+        if(ack == 2){
             _data._m = STOP;
+        }else{
+            delay(4000);
         }
         
         _data._m = _digs->GetValue() == 9999?STOP:_data._m;
@@ -115,5 +140,5 @@ void loop(){
 
         _data._v = (uint16_t)_digs->GetValue();
         _storage.add(_data);
-        _delay_ms(1000);
+        //_delay_ms(1000);
 }
